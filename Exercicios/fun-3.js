b1a13640513605360bb764bf549221dad2c3096e
ianperesdarosa <ianperesdarosa2004@gmail.com>
2022-12-16 01:33:45 -0300

    :fire:

---
+++ Exercicios/fun-3.js
@@ -0,0 +1,4 @@
+function elevado (base, expoente) {
+    console.log(Math.pow(base, expoente));
+}
+elevado(2, 5);
---
+++ Exercicios/fun-4.js
@@ -0,0 +1,4 @@
+function divisao (dividendo, divisor) {
+    console.log(dividendo / divisor, dividendo % divisor);
+}
+divisao(4,7);
