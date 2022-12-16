c3505a04e8c92a2698c17f2a98a7e5e8badae670
ianperesdarosa <ianperesdarosa2004@gmail.com>
2022-12-16 18:09:49 -0300

    :fire:

---
+++ playgroud_do_Javascript/03.js
@@ -0,0 +1,24 @@
+//Repetição
+
+for (let i = 0; i < 10; i++) {
+    console.log("Hello, World!!!")
+}
+
+
+const cartao = {
+    name: "Ian",
+    age: 18,
+    cd: 1857
+}
+//Use em objetos 
+for (i in cartao) {
+    console.log(i, cartao[i])
+}
+
+
+
+const alunos = ["Ana", "Gabi", "Juliha", "Anderson", "João"];
+//So use em arrays
+for (let list of alunos ) {
+    console.log(list)
+}
