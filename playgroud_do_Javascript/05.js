c80be6f6a145d3780c389f2f531ae369cc642b65
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-04-09 13:06:21 -0300

    :fire:

---
+++ playgroud_do_Javascript/05.js
@@ -0,0 +1,15 @@
+function main()  {
+    const num = 4;
+    const process = num % 2;
+
+    if(process = 0) {
+
+        console.log('O número ${num} é Par');
+        
+    } else {
+
+        console.log('O número ${num}é ìmpar');
+    }
+}
+
+main();
